4f52e0ae678052a5b79d87b55f1de76505843df2
editorbhai292-eng <editorbhai292@gmail.com>
2026-04-09 11:51:03 +0530

    Update firebase-config.ts

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,22 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // Ye add kiya
-import { getStorage } from "firebase/storage"; // Ye add kiya
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5EB-4kPO-iqAGlEwzKWJRwAc5g1OCENo",
   authDomain: "marhaba-collection-cloth-shop.firebaseapp.com",
   projectId: "marhaba-collection-cloth-shop",
   storageBucket: "marhaba-collection-cloth-shop.firebasestorage.app",
-  messagingSenderId: "59920508270",
+  messagingSenderId: "599620508270",
   appId: "1:599620508270:web:4f72cdfdd50509652e247f",
   measurementId: "G-RYD8HVQEMJ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore(app); // Database connection
-const storage = getStorage(app); // Storage connection
+const db = getFirestore(app);
+const storage = getStorage(app);
 
 export { app, analytics, db, storage, firebaseConfig };
